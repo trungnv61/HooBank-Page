b846e97963d0f46de818265c5d3131aa4a490fe3
NguyenVanTrung612 <vantrung060101@gmail.com>
2022-08-17 21:07:51 +0700

    add folder components

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,25 @@
+import styles from "./style/style";
+
 const App = () => {
-    return <h1 className="text-red-500">APP</h1>;
+    return (
+        <div className="bg-primary w-full overflow-hidden">
+            <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+                <div className={`${styles.boxWidth}`}>Navbar</div>
+            </div>
+
+            <div className={`bg-primary ${styles.flexStart}`}>
+                <div className={`${styles.boxWidth}`}>Hero</div>
+            </div>
+
+            <div
+                className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}
+            >
+                <div className={`${styles.boxWidth}`}>
+                    Starts Business Billing
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default App;
