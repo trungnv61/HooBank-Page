bca8573e8e92de9883b468666e186b444dc131c5
NguyenVanTrung612 <vantrung060101@gmail.com>
2022-08-20 08:17:31 +0700

    update

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -4,7 +4,18 @@ import { navLinks } from "../../constants";
 const Navbar = () => {
     return (
         <nav className="w-full flex py-6 justify-between items-center navbar">
-            <img src={logo} alt="hoobank" className="w-[124px]" />
+            <img src={logo} alt="hoobank" className="w-[124px] h-[32px]" />
+            <ul className="list-none sm:flex hidden justify-end items-center flex-1">
+                {navLinks.map((nav, index) => (
+                    <li
+                        className={`font-poppins font-normal cursor-pointer text-white text-[16px] ${
+                            index === navLinks.length - 1 ? "mr-0" : "mr-10"
+                        } `}
+                    >
+                        <a href={`#${nav.id}`}>{nav.title}</a>
+                    </li>
+                ))}
+            </ul>
         </nav>
     );
 };
