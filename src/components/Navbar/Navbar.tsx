56b254c2a19e1bf1738bd95e18c47d9a4a7f84f9
NguyenVanTrung612 <vantrung060101@gmail.com>
2022-08-23 16:15:32 +0700

    update

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -6,7 +6,7 @@ const Navbar = () => {
     const [toggle, setToggle] = useState(false);
 
     const handleMenu = () => {
-        setToggle(!toggle);
+        setToggle((prev) => !prev);
     };
 
     return (
@@ -31,25 +31,26 @@ const Navbar = () => {
                     className="w-[28px] h-[28px] object-contain"
                     onClick={handleMenu}
                 />
-            </div>
-
-            <div
-                className={`${
-                    toggle ? "flex" : "hidden"
-                } p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-1 min-w-[140px] rounded-xl sidebar`}
-            >
-                <ul className="list-none flex flex-col justify-end items-center flex-1">
-                    {navLinks.map((nav, index) => (
-                        <li
-                            key={nav.id}
-                            className={`font-poppins font-normal cursor-pointer text-white text-[16px] ${
-                                index === navLinks.length - 1 ? "mr-0" : "mr-10"
-                            } `}
-                        >
-                            <a href={`#${nav.id}`}>{nav.title}</a>
-                        </li>
-                    ))}
-                </ul>
+                <div
+                    className={`${
+                        toggle ? "flex" : "hidden"
+                    } p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-1 min-w-[140px] rounded-xl sidebar`}
+                >
+                    <ul className="list-none flex flex-col justify-end items-center flex-1">
+                        {navLinks.map((nav, index) => (
+                            <li
+                                key={nav.id}
+                                className={`font-poppins font-normal cursor-pointer text-white text-[16px] ${
+                                    index === navLinks.length - 1
+                                        ? "mr-0"
+                                        : "mb-4"
+                                } `}
+                            >
+                                <a href={`#${nav.id}`}>{nav.title}</a>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
             </div>
         </nav>
     );
