f8c7fd615bc1a54011e86d497f15bbb3c76af36f
NguyenVanTrung612 <vantrung060101@gmail.com>
2022-08-23 16:29:15 +0700

    update

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -17,7 +17,7 @@ const Navbar = () => {
                     <li
                         key={nav.id}
                         className={`font-poppins font-normal cursor-pointer text-white text-[16px] ${
-                            index === navLinks.length - 1 ? "mr-0" : "mb-4"
+                            index === navLinks.length - 1 ? "mr-0" : "mr-10"
                         } `}
                     >
                         <a href={`#${nav.id}`}>{nav.title}</a>
